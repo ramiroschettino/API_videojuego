1a10460c22bb54be4ddf6b327e50b5edd140c14d
Ramiro Schettino <ramiroschettino1@hotmail.com>
2024-06-18 20:26:56 -0300

    Haciendo el controller

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,33 @@
+﻿using API_videojuego.Data;
+using API_videojuego.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_videojuego.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public PlayersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreatePlayer(Player player) 
+        {
+            if (player == null)
+            {
+                return BadRequest();
+            }
+            else { 
+                _context.Players.Add(player);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
--- obj/Debug/net8.0/API_videojuego.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_videojuego.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_videojuego")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e316caafa6f421b92e796c516517d86cea3b8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_videojuego")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_videojuego")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
