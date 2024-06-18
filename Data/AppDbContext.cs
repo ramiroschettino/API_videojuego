32e316caafa6f421b92e796c516517d86cea3b8c
Ramiro Schettino <ramiroschettino1@hotmail.com>
2024-06-18 13:43:00 -0300

    commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using API_videojuego.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_videojuego.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
